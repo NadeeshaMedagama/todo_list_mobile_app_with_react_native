65b7d8b2fa341b8906d50775a20f9311993546b8
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-04-24 21:11:36 +0530

    file path changed

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Home from'./src/Home';
+import HomeScreen from'./src/screens/HomeScreen';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => HomeScreen);
